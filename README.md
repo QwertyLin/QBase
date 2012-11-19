796e43f1158ae7ed43d342af5ad4b149e5795dcc
Administrator <Administrator@admin-PC>
2012-11-19 18:10:09 +0800

    Q

---
+++ README.md
@@ -0,0 +1,4 @@
+QBase
+=====
+
+QBase
---
+++ src/q/base/DialogBase.java
@@ -0,0 +1,15 @@
+package q.base;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class DialogBase extends Dialog {
+
+	public DialogBase(Context context) {
+		super(context);
+		getWindow().setBackgroundDrawableResource(R.drawable.base_transparent);
+		
+	}
+	
+
+}
