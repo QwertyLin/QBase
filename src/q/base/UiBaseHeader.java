f3c293504a8cb466e3d19f38946bc877a79566ba
Administrator <Administrator@admin-PC>
2012-11-30 18:36:02 +0800

    Q

---
+++ src/q/base/UiBaseHeader.java
@@ -0,0 +1,36 @@
+package q.base;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class UiBaseHeader {
+	
+	public static final void text(final Activity act, String text){
+		((TextView)act.findViewById(R.id.base_header_text)).setText(text);
+	}
+		
+	public static final void btnBack(final Activity act, String text){
+		text(act, text);
+		ImageButton btn = (ImageButton)act.findViewById(R.id.base_header_btn_left);
+		btn.setVisibility(View.VISIBLE);
+		btn.setImageResource(R.drawable.a_navigation_back);
+		btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				act.finish();
+			}
+		});
+	}
+	
+	public static final void btnBackOk(final Activity act, String text, OnClickListener onClickOk){
+		btnBack(act, text);
+		ImageButton btn = (ImageButton)act.findViewById(R.id.base_header_btn_right);
+		btn.setVisibility(View.VISIBLE);
+		btn.setImageResource(R.drawable.a_navigation_accept);
+		btn.setOnClickListener(onClickOk);
+	}
+
+}
